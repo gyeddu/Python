50d57d97f8de3d9b27f5ff04bba96c3319986cc2
gyeddu <ygovindarajulu93@gmail.com>
2023-11-29 14:30:19 -0800

    variable

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Variable is like a container that holds data. 
+# Very similar to how our containers in kitchen holds sugar, salt etc 
+# Creating a variable is like creating a placeholder in memory and assigning it some value.
+# Variable must start with underscore(_) or a letter
+# Variable must not start with a number or a special characters
+# Variables names are case-sensitive 
+a=b=c=100
+print(a,b,c)
+
+raju=200
+print(raju)
+
+a,b,c=1,2,3
+print(a,b,c)
+
+a,b,c=100,200,400
+print(a+b+c)
+
+sai123=321
+print(sai123)
+
+SAi=1
+print(SAi)
+
+raju=100
+print(raju)
