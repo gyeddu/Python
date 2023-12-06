7d55138c551bcc2e8cd275cad9f2532220c2dcac
gyeddu <ygovindarajulu93@gmail.com>
2023-12-05 22:00:20 -0800

    int

--- var.py
+++ var.py
@@ -11,3 +11,17 @@ print(type(a), type(b),type(c),type(d),type(e),type(f),type(g))
 # PS C:\Users\princ\OneDrive\Documents\Python Code\Python> python3 var.py                                                                                        
 # <class 'int'> <class 'int'> <class 'float'> <class 'float'> <class 'bool'> <class 'bool'> <class 'complex'>
 # PS C:\Users\princ\OneDrive\Documents\Python Code\Python>
+
+a=99.9999
+print(int(a))
+
+d=float(input("enter something:"))
+print(d)
+
+# PS C:\Users\princ\OneDrive\Documents\Python Code\Python> python3 var.py
+# <class 'int'> <class 'int'> <class 'float'> <class 'float'> <class 'bool'> <class 'bool'> <class 'complex'>
+# 99
+# enter something:2.2
+# 2.2
+# PS C:\Users\princ\OneDrive\Documents\Python Code\Python> 
+
