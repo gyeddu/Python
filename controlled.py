7f2c8ff0d8b93bd0599fc1b7939f787d0203a7e2
gyeddu <ygovindarajulu93@gmail.com>
2024-02-20 19:03:49 -0800

    change

---
+++ controlled.py
@@ -0,0 +1,4 @@
+# Basic of Python
+# What is Operators
+# Python Operators in general are used to perform operations on values and variables. Special symbols Eg- + , * , -, /, etc
+# Operands are also know as Values
