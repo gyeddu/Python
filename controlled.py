e8d5d9baa1a4db61d56178af8b637c1daa573e5e
gyeddu <ygovindarajulu93@gmail.com>
2024-07-31 10:45:49 -0700

    Controlled

--- controlled.py
+++ controlled.py
@@ -2,3 +2,12 @@
 # What is Operators
 # Python Operators in general are used to perform operations on values and variables. Special symbols Eg- + , * , -, /, etc
 # Operands are also know as Values
+# Arithmetic Operator :- Used to perform mathematical operations like addition, subtraction, multiplication etc.
+# Comparison operator :- Want to compare a value with another value. To do that you use Comparison operator. It returns either true or false according to the condation.
+# Ex :- 2<5 :- contition True
+# Python Assignment operator :- Used in python to assign values to variable. va=54 is a simple assignment operator that assign value 54 
+# Logical Operator :- Used on conditional statements ( either True or false). They perform Logical AND (-), Logical OR(+), and Logical NOT operations.
+# Control Statements :- 
+# conditional Statemets also known as Decision Making statements
+# Transfer Statemets also known as Jumping statements
+# Iterative Statemets also known as Looping Statements
